9a09558a0b56dde08ee5ec25235dc471d10b53f5
GuilhermeMarques1 <g-marques@outlook.com>
2023-10-03 17:33:09 -0300

    dev: Config fonts

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
+
 import { ThemeProvider } from "styled-components";
 
 import theme from "@theme/index";
@@ -5,9 +8,16 @@ import theme from "@theme/index";
 import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {
+        fontsLoaded ? <Groups /> : <ActivityIndicator />
+      }
     </ThemeProvider>
   );
 }
