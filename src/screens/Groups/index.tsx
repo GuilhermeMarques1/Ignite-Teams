ffc8f5e58dacb21f54a97e2c06823a42c2dcd318
GuilhermeMarques1 <g-marques@outlook.com>
2023-10-26 01:47:55 -0300

    feat: Add list for groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 
@@ -5,6 +8,8 @@ import { Container } from './styles';
 import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Amigos', 'Familia', 'Grupo da faculdade', 'Trabalho', 'Futebol da comp', 'Lolzin dos crias', 'Os pexe']);
+
   return (
     <Container>
       <Header showBackButton/>
@@ -14,7 +19,15 @@ export function Groups() {
         subtitle="jogue com a sua turma"
       />
 
-      <GroupCard title="Galera do Ignite" />
+      {/* <GroupCard title="Galera do Ignite" /> */}
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
+
     </Container>
   );
 }
