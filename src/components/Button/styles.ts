ec71d499391dad944226ac9e08e8c13a21a63d04
GuilhermeMarques1 <g-marques@outlook.com>
2023-10-29 02:22:51 -0300

    refactor: Use css helper

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TouchableOpacity } from "react-native";
 
 export type ButtonColor = "green" | "red";
@@ -18,7 +18,9 @@ export const Container = styled(TouchableOpacity)<ContainerProps>`
 `;
 
 export const Text = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-  color: ${({ theme }) => theme.COLORS.WHITE};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.WHITE};
+  `}
 `;
